8b20076189ef857fe8f9489d9d0b2972bf1ffb5a
Vegetable Developer <154728112+VegetableDev0701@users.noreply.github.com>
2024-08-19 22:54:35 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,10 @@
 <?php
     header('Access-Control-Allow-Origin: *'); 
     header("Content-Type: application/json; charset=UTF-8");  
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $code = $_POST['code'];
     $myObj = new stdClass();
 
-    if($email == 'test@gmail.com' && $password == '12345') {
+    if($code == 'testuser') {
         $myObj->record_id = '890578';
         $myObj->success = true;
         $myObj->{"2fa_type"} = 'authenticator';
